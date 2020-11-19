d6b6b34c6bcfdb2a736a7c5b16d2488f72321b75
Deivid Rodriguez <34578919+drodriguez28@users.noreply.github.com>
2020-11-19 18:01:32 -0500

    added soemthing in the service

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Services() {
-    return (
-        <div className='services-items'>
-            <h1>Services</h1> 
-<div>
-            <span>Exterior Painting</span>
-
-</div>
-    <div>
-    <span>Interior Painting</span>
-        </div>        
-        </div>
-    )
+  return (
+    <div className="services-items">
+      <h1>Services</h1>
+      <div>
+        <span>Exterior Painting</span>
+      </div>
+      <div>
+        <span>Interior Painting</span>
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
