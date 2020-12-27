b3ff6ccb165f6c738118ac8e2ae411ff47f45146
Deivid Rodriguez <34578919+drodriguez28@users.noreply.github.com>
2020-12-26 22:26:43 -0500

    added cards of ex and in for services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,15 +1,42 @@
 import React from "react";
+import { Card, Row, Col } from "antd";
+
+const { Meta } = Card;
 
 function Services() {
   return (
     <div className="services-items">
       <h1>Services</h1>
-      <div>
-        <span>Exterior Painting</span>
-      </div>
-      <div>
-        <span>Interior Painting</span>
-      </div>
+      <Row>
+        <Col span={6}>
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={
+              <img
+                alt="example"
+                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              />
+            }
+          >
+            <Meta title="Interior Painting " description="www.instagram.com" />
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={
+              <img
+                alt="example"
+                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              />
+            }
+          >
+            <Meta title="Exterior Painting " description="www.instagram.com" />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
