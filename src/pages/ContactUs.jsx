bd62bb113e1ef9a5f8321508a9c9ed1c0408f124
Deivid Rodriguez <34578919+drodriguez28@users.noreply.github.com>
2021-02-03 17:45:16 -0500

    added contact page and css

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Card, Form, Input, Radio } from "antd";
+
+const { TextArea } = Input;
 
 function ContactUs() {
-    return (
-        <div className="contact-us-items">
-            <h1>Contact Us</h1>
-            
-        </div>
-    )
+  const contactMethodOptions = [
+    { label: "Email", value: "Email" },
+    { label: "Phone", value: "Phone" },
+  ];
+
+  return (
+    <div className="contact-us-items">
+      <h1>Contact Us</h1>
+      <Row className="painting-contact-card">
+        <Col span={15}>
+          <Card
+            title="Free Contactless Quote"
+            style={{ width: 300 }}
+            headStyle={{ fontWeight: "bold" }}
+          >
+            <Row>
+              <Col span={24}>
+                <Form.Item label="Client Name" style={{ fontWeight: "bold" }}>
+                  <Input style={{ width: 200 }}></Input>
+                </Form.Item>
+                <Form.Item label="Email Address" style={{ fontWeight: "bold" }}>
+                  <Input style={{ width: 200 }}></Input>
+                </Form.Item>
+                <Form.Item label="Phone Number" style={{ fontWeight: "bold" }}>
+                  <Input style={{ width: 200 }}></Input>
+                </Form.Item>
+                <Form.Item label="Zip Code" style={{ fontWeight: "bold" }}>
+                  <Input style={{ width: 200 }}></Input>
+                </Form.Item>
+
+                <Form.Item
+                  label="Preferred Contact Method"
+                  style={{ fontWeight: "bold" }}
+                >
+                  <Radio.Group options={contactMethodOptions}></Radio.Group>
+                </Form.Item>
+
+                <Form.Item
+                  label=" Additional Messages"
+                  style={{ fontWeight: "bold" }}
+                >
+                  <TextArea rows={4}></TextArea>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
