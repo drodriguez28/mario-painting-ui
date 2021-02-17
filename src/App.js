8efa118317ddf49d182d57d93201c07dd92113e5
Deivid Rodriguez <34578919+drodriguez28@users.noreply.github.com>
2021-02-17 17:49:12 -0500

    added new routes and ripped out nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Layout, Menu } from "antd";
+// import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import ContactUs from './pages/ContactUs';
+
+
+const { Header, Content, Footer } = Layout;
+
+
+
 function App() {
   return (
     <>
       <Router>
-        <NavBar />
+        {/* <NavBar /> */}
+        <Layout >
 
-        <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/services' exact component={Services}/>
-          <Route path='/contact-us' exact component={ContactUs}/>
-        </Switch>
+          <Menu theme="dark" mode="horizontal">
+            <Menu.Item key="1">
+              <Link to="/">
+                <span>Home</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="2" >
+              <Link to="/services">
+                <span>Services</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="3" >
+              <Link to="/contact-us">
+                <span>Contact Us</span>
+              </Link>
+            </Menu.Item>
+          </Menu>
+
+          <Layout>
+            <Header style={{ background: "#fff", padding: 0 }} />
+            <Content >
+              <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+              <Switch>
+                <Route path='/' exact component={Home} />
+
+              </Switch>
+              <Switch>
+                <Route path='/services' exact component={Services} />
+
+              </Switch>
+
+              <Switch>
+                <Route path='/contact-us' exact component={ContactUs} />
+
+              </Switch>
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+              Mario's Painting Roswell ©2021 Created by Deivid Rodriguez
+            </Footer>
+
+          </Layout>
+
+
+        </Layout>
       </Router>
     </>
   );
 }
 
 export default App;
+
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-import { Layout, Menu, Carousel } from "antd";
-
-const { Header, Content, Footer } = Layout;
+import { Carousel } from "antd";
 
 function Home() {
   const contentStyle = {
@@ -14,55 +12,43 @@ function Home() {
   };
 
   return (
-    <Layout className="layout">
-      <Header >
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal">
-          <Menu.Item key="1">Home </Menu.Item>
-          <Menu.Item key="2" >Services</Menu.Item>
-          <Menu.Item key="3">Contact Us</Menu.Item>
-        </Menu>
-      </Header>
+    <>
       <div className="home-items">
-      <h1>
-        <img src="./logo.jpeg" />
-      </h1>
+        <h1>
+          <img src="./logo.jpeg" />
+        </h1>
       </div>
-      <Content style={{ padding: "0 50px" }}>
-        <Carousel autoplay>
-          <div>
-            <h3 style={contentStyle}>Welcome to Mario's Painting Roswell</h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>
-              Schedule a time to meet a painting professional at your home.
-              We’ll hear out all your needs and get right to work creating a
-              quote for your home.
-            </h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>
-              Get a clear scope of the project in your inbox with specified
-              paint colors, materials and exact pricing.
-            </h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>
-              Sign your quote and select your start date. Complete a small
-              checklist to ensure a smooth painting process.
-            </h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>
-              Checkout the work and let us know if you were satified!
-            </h3>
-          </div>
-        </Carousel>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Mario's Painting Roswell ©2021 Created by Deivid Rodriguez
-      </Footer>
-    </Layout>
+
+      <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle}>Welcome to Mario's Painting Roswell</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>
+            Schedule a time to meet a painting professional at your home. We’ll
+            hear out all your needs and get right to work creating a quote for
+            your home.
+          </h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>
+            Get a clear scope of the project in your inbox with specified paint
+            colors, materials and exact pricing.
+          </h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>
+            Sign your quote and select your start date. Complete a small
+            checklist to ensure a smooth painting process.
+          </h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>
+            Checkout the work and let us know if you were satified!
+          </h3>
+        </div>
+      </Carousel>
+    </>
   );
 }
 
