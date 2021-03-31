0c03b284c9774104cf0130d7be8015f7020c3f14
Deivid Rodriguez <34578919+drodriguez28@users.noreply.github.com>
2021-03-31 13:52:41 -0400

    app is responsive adding contact us and css

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -62,73 +62,65 @@ const ContactUs = () => {
 
       {displaySuccessAlert()}
       <Row className="painting-contact-card">
-        <Col span={10}>
+        <Col xs={24} xl={8}>
           <Card
             title="Insert Info for an Estimate !"
-            style={{ width: 600 }}
             headStyle={{ fontWeight: "bold" }}
           >
-            <Row>
-              <Col span={25}>
-                <Form.Item label="First Name" style={{ fontWeight: "bold" }}>
-                  <Input style={{ width: 200 }}></Input>
-                </Form.Item>
-                <Form.Item label="Last Name" style={{ fontWeight: "bold" }}>
-                  <Input style={{ width: 200 }}></Input>
-                </Form.Item>
-                <Form.Item label="Email Address" style={{ fontWeight: "bold" }}>
-                  <Input style={{ width: 200 }}></Input>
-                </Form.Item>
-                <Form.Item
-                  label=" Street Address"
-                  style={{ fontWeight: "bold" }}
-                >
-                  <Input style={{ width: 200 }}></Input>
-                </Form.Item>
-                <Form.Item label="City" style={{ fontWeight: "bold" }}>
-                  <Input style={{ width: 200 }}></Input>
-                </Form.Item>
-                <Form.Item label="Zip Code" style={{ fontWeight: "bold" }}>
-                  <Input style={{ width: 160 }}></Input>
-                </Form.Item>
-                <Form.Item label="Phone Number" style={{ fontWeight: "bold" }}>
-                  <Input style={{ width: 200 }}></Input>
-                </Form.Item>
+            <Form.Item label="First Name" style={{ fontWeight: "bold" }}>
+              <Input style={{ width: 300 }}></Input>
+            </Form.Item>
+            <Form.Item label="Last Name" style={{ fontWeight: "bold" }}>
+              <Input style={{ width: 300 }}></Input>
+            </Form.Item>
+            <Form.Item label="Email Address" style={{ fontWeight: "bold" }}>
+              <Input style={{ width: 300 }}></Input>
+            </Form.Item>
+            <Form.Item label=" Street Address" style={{ fontWeight: "bold" }}>
+              <Input style={{ width: 300 }}></Input>
+            </Form.Item>
+            <Form.Item label="City" style={{ fontWeight: "bold" }}>
+              <Input style={{ width: 300 }}></Input>
+            </Form.Item>
+            <Form.Item label="Zip Code" style={{ fontWeight: "bold" }}>
+              <Input style={{ width: 160 }}></Input>
+            </Form.Item>
+            <Form.Item label="Phone Number" style={{ fontWeight: "bold" }}>
+              <Input style={{ width: 300 }}></Input>
+            </Form.Item>
 
-                <Form.Item
-                  label="Preferred Contact Method"
-                  style={{ fontWeight: "bold" }}
-                >
-                  <Radio.Group options={contactMethodOptions}></Radio.Group>
-                </Form.Item>
+            <Form.Item
+              label="Preferred Contact Method"
+              style={{ fontWeight: "bold" }}
+            >
+              <Radio.Group options={contactMethodOptions}></Radio.Group>
+            </Form.Item>
 
-                <Form.Item
-                  label="Type of Painting Service (if both check both)"
-                  style={{ fontWeight: "bold" }}
-                >
-                  <Checkbox.Group options={typeofServiceOptions} />
-                </Form.Item>
+            <Form.Item
+              label="Type of Painting Service (if both check both)"
+              style={{ fontWeight: "bold" }}
+            >
+              <Checkbox.Group options={typeofServiceOptions} />
+            </Form.Item>
 
-                <Form.Item
-                  label=" Additional Messages"
-                  style={{ fontWeight: "bold" }}
-                >
-                  <TextArea rows={4}></TextArea>
-                </Form.Item>
+            <Form.Item
+              label=" Additional Messages"
+              style={{ fontWeight: "bold" }}
+            >
+              <TextArea rows={4}></TextArea>
+            </Form.Item>
 
-                <Button type="primary" onClick={showModal}>
-                  Submit
-                </Button>
-                <Modal
-                  title="Please confirm"
-                  visible={isModalVisible}
-                  onOk={handleOk}
-                  onCancel={handleCancel}
-                >
-                  <p>Are you sure the information you provided is correct ?</p>
-                </Modal>
-              </Col>
-            </Row>
+            <Button type="primary" onClick={showModal}>
+              Submit
+            </Button>
+            <Modal
+              title="Please confirm"
+              visible={isModalVisible}
+              onOk={handleOk}
+              onCancel={handleCancel}
+            >
+              <p>Are you sure the information you provided is correct ?</p>
+            </Modal>
           </Card>
         </Col>
       </Row>
